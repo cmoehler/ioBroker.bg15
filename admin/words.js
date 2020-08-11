5613e5b477d2c5d9a81490d08adb2bd013b7722e
cmoehler <ccm@gmx.net>
2020-08-11 13:58:26 +0200

    first

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"bg15 adapter settings": {
+		"en": "Adapter settings for bg15",
+		"de": "Adaptereinstellungen für bg15",
+		"ru": "Настройки адаптера для bg15",
+		"pt": "Configurações do adaptador para bg15",
+		"nl": "Adapterinstellingen voor bg15",
+		"fr": "Paramètres d'adaptateur pour bg15",
+		"it": "Impostazioni dell'adattatore per bg15",
+		"es": "Ajustes del adaptador para bg15",
+		"pl": "Ustawienia adaptera dla bg15",
+		"zh-cn": "bg15的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
